28ac8a263004a7dda9aa67e55b7e27802265a8c7
donhyuck <leedonhyuck123@gmail.com>
2022-08-07 16:03:56 +0900

    프로젝트 세팅

---
+++ src/main/java/com/ldh/exam/demo/controller/UserHomeController.java
@@ -0,0 +1,16 @@
+package com.ldh.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserHomeController {
+
+	@RequestMapping("/user/main")
+	@ResponseBody
+	public String showMain() {
+		return "HI";
+	}
+
+}

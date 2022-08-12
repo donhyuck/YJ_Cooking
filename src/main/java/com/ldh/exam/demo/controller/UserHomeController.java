c164ec0a331092f1316902c6798a04204972e47d
donhyuck <leedonhyuck123@gmail.com>
2022-08-12 18:15:22 +0900

    레시피목록 디자인 02/ 상단 메뉴바에서 이동

--- src/main/java/com/ldh/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/ldh/exam/demo/controller/UserHomeController.java
@@ -7,14 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserHomeController {
 
-	@RequestMapping("/user/home/main")
-	public String showMain() {
-		return "/user/home/main";
-	}
-
 	@RequestMapping("/")
 	public String showRoot() {
-		return "redirect:/user/home/main";
+		return "redirect:/user/list/suggest";
 	}
 
 }
